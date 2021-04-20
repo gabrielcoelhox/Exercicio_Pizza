dbd2139a830e8658c98780c083a9bd06b5342fe9
unknown <gabriel.coelhox@gmail.com>
2021-04-20 17:52:41 -0300

    Adicionando identificador ao carrinho de compras

--- script.js
+++ script.js
@@ -81,11 +81,20 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 c('.pizzaInfo--addButton').addEventListener('click', () => {
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
 
-    cart.push({
-        id:pizzaJson[modalKey].id,
-        size,
-        quantidade:modalQT
-    });
+    let identificador = pizzaJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item)=>item.identificador == identificador);
+
+    if(key > -1){
+        cart[key].quantidade += modalQT;
+    } else {
+        cart.push({
+            identificador,
+            id:pizzaJson[modalKey].id,
+            size,
+            quantidade:modalQT
+        });
+    }
 
     closeModal();
 });
