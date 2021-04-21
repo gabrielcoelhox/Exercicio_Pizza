7f7fcdba9cf91f22dde98b992181f6781ad85e6a
unknown <gabriel.coelhox@gmail.com>
2021-04-21 17:09:59 -0300

    Preenchendo itens do carrinho

--- script.js
+++ script.js
@@ -101,11 +101,36 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
 function updateCart() {
     if(cart.length > 0) {
         c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
+
         for(let i in cart){
 
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            let cartItem = c('.models .cart--item').cloneNode(true);
 
-                console.log(pizzaItem);
+            // Imagem e nome da pizza no carrinho
+            let pizzaSizeName;
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+
+            // Quantidade de pizzas no carrinho
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].quantidade;
+
+            c('.cart').append(cartItem);
         }
     } else {
         c('aside').classList.remove('show');
