ed064069b5ad84109c7cbb371d45e709f4b81575
unknown <gabriel.coelhox@gmail.com>
2021-04-21 17:27:52 -0300

    Adicionando quantidade, preço e desconto ao manu do carrinho.

--- script.js
+++ script.js
@@ -103,9 +103,15 @@ function updateCart() {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
 
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart){
 
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].quantidade;
+
             let cartItem = c('.models .cart--item').cloneNode(true);
 
             // Imagem e nome da pizza no carrinho
@@ -126,12 +132,29 @@ function updateCart() {
 
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
-
-            // Quantidade de pizzas no carrinho
-            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].quantidade;
-
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].quantidade; // Quantidade de pizzas no carrinho
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].quantidade > 1){
+                    cart[i].quantidade--; 
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].quantidade++;
+                updateCart();
+            });
             c('.cart').append(cartItem);
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
     } else {
         c('aside').classList.remove('show');
     }
